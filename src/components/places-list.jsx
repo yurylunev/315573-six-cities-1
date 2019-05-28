88643236e86fe3254ce069184440b67fad67d2c8
Yury Lunev <yury.lunev@me.com>
2019-05-28 10:38:08 +0300

    Исправлено лишнее кэширование

--- src/components/places-list.jsx
+++ src/components/places-list.jsx
@@ -13,18 +13,9 @@ class PlacesList extends React.PureComponent {
   render() {
     return <div className="cities__places-list places__list tabs__content">
       {this.props.placesList.map((place, i) => {
-        const offer = {
-          mark: place.mark,
-          imageURL: place.imageURL,
-          price: place.price,
-          currency: place.currency,
-          rate: place.rate,
-          name: place.name,
-          type: place.type
-        };
         return <PlaceCard
           key={i}
-          offer={offer}
+          offer={place}
           clickHandler={(clickOffer) => {
             this.setState({activeCard: clickOffer});
           }}
