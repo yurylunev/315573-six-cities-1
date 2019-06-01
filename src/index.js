add7ac1b8baac6845201aff0f4aa502b6ff03738
Yury Lunev <yury.lunev@me.com>
2019-06-01 16:14:28 +0300

    3. Инициализировать новое хранилище

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {reducer} from "./reducer";
+
 import App from './components/app.jsx';
 import offers from './mocks/offers.js';
+
+const store = createStore(reducer);
 
 const init = (currentOffers) => {
   ReactDOM.render(
