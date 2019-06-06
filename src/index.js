b24f574058d7577b63d39e13fe40ae94165fc488
Yury Lunev <yury.lunev@me.com>
2019-06-06 19:13:35 +0300

    2. Обновить код инициализации хранилища Redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import {reducer} from "./reducer";
 import {Provider} from "react-redux";
 import App from './components/app.jsx';
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 
 const init = () => {
   ReactDOM.render(<Provider store={store}>
