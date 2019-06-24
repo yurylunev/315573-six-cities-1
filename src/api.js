eb9d86badebc9b4831ccb5c70464cf063039e08e
Yury Lunev <yury.lunev@me.com>
2019-06-24 14:33:12 +0300

    3. Создать новый модуль api.js

---
+++ src/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import {ActionCreators} from "./reducer";
+
+const createAPI = (dispatch) => {
+  const api = axios.create({
+    baseURL: `https://es31-server.appspot.com/six-cities`,
+    timeout: 5000,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => response;
+  const onFail = (err) => {
+    if (err.response.status === 403) {
+      dispatch(ActionCreators.requireAuthorization(true));
+    }
+    return err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+  return api;
+};
+
+export default createAPI;
