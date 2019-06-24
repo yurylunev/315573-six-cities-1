fed4309ce71b17359b882ceda5ccce3ffed2ca87
Yury Lunev <yury.lunev@me.com>
2019-06-24 15:36:30 +0300

    6. Обновить код инициализации хранилища, добавив thunk

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ import thunk from 'redux-thunk';
 import {compose} from 'recompose';
 import createAPI from "./api";
 
-const store = createStore(
-    reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
-
 const init = () => {
   const api = createAPI((...args) => store.dispatch(...args));
+  const store = createStore(
+      reducer, compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      )
+  );
+
   ReactDOM.render(<Provider store={store}>
     <App/>
   </Provider>, document.getElementById(`root`));
