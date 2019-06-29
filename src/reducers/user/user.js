cecc320b250e932d0bddf540223bb09f637a27f6
Yury Lunev <yury.lunev@me.com>
2019-06-30 01:02:48 +0300

    5. Сохранить полученную информацию о пользователе в store

--- src/reducers/user/user.js
+++ src/reducers/user/user.js
@@ -3,13 +3,18 @@ const initialState = Object.assign({}, {
 });
 
 const ActionType = {
-  UPDATE_AUTHORIZATION: `UPDATE_AUTHORIZATION`
+  UPDATE_AUTHORIZATION: `UPDATE_AUTHORIZATION`,
+  UPDATE_USER_INFO: `UPDATE_USER_INFO`
 };
 
 const ActionCreator = {
   'requireAuthorization': (status) => ({
     type: ActionType.UPDATE_AUTHORIZATION,
     payload: status
+  }),
+  'saveUserInfo': (info) => ({
+    type: ActionType.UPDATE_USER_INFO,
+    payload: info
   })
 };
 
@@ -17,7 +22,10 @@ const Operation = {
   loginAsync: (data) => (dispatch, _getState, api) => {
     return api.post(`/login`, data)
       .then((response) => {
-        dispatch(ActionCreator.requireAuthorization(response.status !== 200));
+        if (response.status === 200) {
+          dispatch(ActionCreator.requireAuthorization(false));
+          dispatch(ActionCreator.saveUserInfo(response.data));
+        }
       });
   }
 };
@@ -26,6 +34,14 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.UPDATE_AUTHORIZATION:
       return Object.assign({}, state, {isAuthorizationRequired: action.payload});
+    case ActionType.UPDATE_USER_INFO:
+      return Object.assign({}, state, {
+        id: action.payload.id,
+        email: action.payload.email,
+        name: action.payload.name,
+        avatarUrl: action.payload.avatar_url,
+        isPro: action.payload.is_pro
+      });
   }
   return state;
 };
