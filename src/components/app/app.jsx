14c1a9a3683c9ce80fb63a75b8044a03c2260fc7
Yury Lunev <yury.lunev@me.com>
2019-06-29 19:06:05 +0300

    4. Изменить логику рендера компонента «Главная страница»

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,6 +6,7 @@ import CitiesList from "../cities-list/cities-list.jsx";
 import {connect} from "react-redux";
 import {Operation as DataOperation} from "../../reducers/data/data";
 import {getCurrentCity} from "../../reducers/selectors";
+import SignIn from "../sign-in/sign-in.jsx";
 
 export class App extends React.PureComponent {
   constructor(props) {
@@ -18,70 +19,74 @@ export class App extends React.PureComponent {
   }
 
   render() {
-    if (!this.props.loaded) {
-      return <h1>Loading ... </h1>;
-    }
-    return <React.Fragment>
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
-      <main className="page__main page__main--index">
-        <h1 className="visually-hidden">Cities</h1>
-        <div className="cities tabs">
-          <CitiesList/>
-        </div>
-        <div className="cities__places-wrapper">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{this.props.currentCity.offersCount} places to stay
-                in {this.props.currentCity.cityName}</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"/>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
+    if (this.props.isAuthorizationRequired) {
+      return <SignIn/>;
+    } else {
+      if (!this.props.loaded) {
+        return <h1>Loading ... </h1>;
+      }
+      return <React.Fragment>
+        <header className="header">
+          <div className="container">
+            <div className="header__wrapper">
+              <div className="header__left">
+                <a className="header__logo-link header__logo-link--active">
+                  <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+                </a>
+              </div>
+              <nav className="header__nav">
+                <ul className="header__nav-list">
+                  <li className="header__nav-item user">
+                    <a className="header__nav-link header__nav-link--profile" href="#">
+                      <div className="header__avatar-wrapper user__avatar-wrapper">
+                      </div>
+                      <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                    </a>
+                  </li>
                 </ul>
-              </form>
-              <PlacesList/>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map">
-                <CitiesMap key={this.props.currentCity.id}/>
-              </section>
+              </nav>
             </div>
           </div>
-        </div>
+        </header>
 
-      </main>
-    </React.Fragment>;
+        <main className="page__main page__main--index">
+          <h1 className="visually-hidden">Cities</h1>
+          <div className="cities tabs">
+            <CitiesList/>
+          </div>
+          <div className="cities__places-wrapper">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{this.props.currentCity.offersCount} places to stay
+                  in {this.props.currentCity.cityName}</b>
+                <form className="places__sorting" action="#" method="get">
+                  <span className="places__sorting-caption">Sort by</span>
+                  <span className="places__sorting-type" tabIndex="0">Popular
+                    <svg className="places__sorting-arrow" width="7" height="4">
+                      <use xlinkHref="#icon-arrow-select"/>
+                    </svg>
+                  </span>
+                  <ul className="places__options places__options--custom places__options--opened">
+                    <li className="places__option places__option--active" tabIndex="0">Popular</li>
+                    <li className="places__option" tabIndex="0">Price: low to high</li>
+                    <li className="places__option" tabIndex="0">Price: high to low</li>
+                    <li className="places__option" tabIndex="0">Top rated first</li>
+                  </ul>
+                </form>
+                <PlacesList/>
+              </section>
+              <div className="cities__right-section">
+                <section className="cities__map map">
+                  <CitiesMap key={this.props.currentCity.id}/>
+                </section>
+              </div>
+            </div>
+          </div>
+
+        </main>
+      </React.Fragment>;
+    }
   }
 }
 
@@ -92,7 +97,8 @@ App.propTypes = {
     cityName: PropTypes.string.isRequired,
     offersCount: PropTypes.number.isRequired
   }),
-  loadOffersAsync: PropTypes.func.isRequired
+  loadOffersAsync: PropTypes.func.isRequired,
+  isAuthorizationRequired: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = (state) => Object.assign({}, state, {
@@ -101,7 +107,8 @@ const mapStateToProps = (state) => Object.assign({}, state, {
     cityName: ``,
     offersCount: 0
   },
-  loaded: state.DATA.loaded
+  loaded: state.DATA.loaded,
+  isAuthorizationRequired: state.APP.isAuthorizationRequired
 });
 
 const mapDispatchToProps = (dispatch) => ({
